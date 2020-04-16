e0395a3a39c2b5d0b547632513a579ac643c88f6
kvvchetty <kvvchetty@outlook.com>
2020-04-16 18:20:15 -0400

    initial commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://localhost:27017/bezkoder_db"
+  url: "mongodb+srv://new-user-31:eYuSLfB40Vkg6BQE@cluster0-5xy27.gcp.mongodb.net/test?retryWrites=true&w=majority"
 };
