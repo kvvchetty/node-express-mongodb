8c743bb3cbe77438851b7692bc4f9ef8406944cc
kvvchetty <kvvchetty@outlook.com>
2020-04-18 07:52:22 -0400

    csrf fix

--- server.js
+++ server.js
@@ -4,12 +4,35 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
+//var corsOptions = {
 //  origin: "http://localhost:8081"
-  origin: "https://agcurdappxk01.herokuapp.com"
-};
+//  origin: "https://agcurdappxk01.herokuapp.com"
+//};
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://agcurdappxk01.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
